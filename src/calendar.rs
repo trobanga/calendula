5a02472d045b2d44bd7074699cb23a47ebe996f4
Daniel Hahne <d@niel.re>
2024-03-24 15:11:08 +0100

    Refactor and restyle

--- src/calendar.rs
+++ src/calendar.rs
@@ -1,7 +1,6 @@
 use getset::{Getters, Setters};
 
 use chrono::prelude::*;
-use leptos_router::MultiActionForm;
 use serde::{Deserialize, Serialize};
 
 use leptos::*;
@@ -33,30 +32,19 @@ pub struct CalendarEntry {
 /// Renders the home page of your application.
 #[component]
 pub fn Calendar() -> impl IntoView {
-    let add_calendar_entry = create_server_multi_action::<AddCalendarEntry>();
-    let entries_resource = create_resource(
-        move || add_calendar_entry.version().get(),
-        move |_| calendar(),
-    );
-
-    logging::log!("Build calendar");
-    let now = Local::now().date_naive().to_string();
-
+    let (done, set_done) = create_signal(0);
     // let mut current_date = None;
     view! {
-        <div>
-            <MultiActionForm action=add_calendar_entry>
-                <label>
-                    "Add a CalendarEntry"
-                    <br/>
-                    <input type="date" name="start_date" value=&now min=&now/>
-                    <input type="time" name="start_time"/>
-                    // <input type="checkbox" name="whole_day"/>
-                    <br/>
-                    <input type="text" name="title"/>
-                </label>
-                <input type="submit" value="Add"/>
-            </MultiActionForm>
+        <NewEntry set_done=set_done/>
+        <CalendarEntries done=done set_done=set_done/>
+    }
+}
+
+#[component]
+pub fn CalendarEntries(done: ReadSignal<usize>, set_done: WriteSignal<usize>) -> impl IntoView {
+    let entries_resource = create_resource(done, move |_| calendar());
+    view! {
+        <div class="mx-auto">
             <Transition fallback=move || view! {<p>"Loading..."</p> }>
                 <ErrorBoundary fallback=|errors| view!{<ErrorTemplate errors=errors/>}>
                     {move || {
@@ -67,26 +55,81 @@ pub fn Calendar() -> impl IntoView {
                                    }
                                    Ok(entries) => {
                                        entries.into_iter()
-                                              .map(move |entry|
-                                                   view! {
-                                                       <p class="text-amber-600">{entry.title()}: {entry.start_date().to_string()} - {entry.start_time().map(|t| t.to_string()).unwrap_or_default()}</p>
-                                                       <button on:click=move |_| {
-                                                           let id = *entry.id();
-                                                           logging::log!("click {id}");
-                                                           spawn_local(async move {
-                                                               if delete_calendar_entry(id).await.is_err() {logging::warn!("Cannot delete");};
-                                                               entries_resource.refetch();
-                                                           });
-                                                       }>
-                                                           Delete
-                                                       </button>
-                                       }).collect_view()
+                                              .map(move |entry| view!{<CalendarEntry entry=entry set_done=set_done/>})
+                                              .collect_view()
                                    }
                     }).unwrap_or_default()
                     }}
-
                 </ErrorBoundary>
             </Transition>
+        </div>
+    }
+}
+
+#[component]
+pub fn CalendarEntry(entry: CalendarEntry, set_done: WriteSignal<usize>) -> impl IntoView {
+    let entry_id = entry.id().clone();
+    view! {
+        <div class="">
+            <p class="text-amber-600">
+                {entry.title()}: {entry.start_date().to_string()} - {entry.start_time().map(|t| t.to_string()).unwrap_or_default()}
+            </p>
+            <button on:click=move |_| {
+                let id = entry_id;
+                spawn_local(async move {
+                    if delete_calendar_entry(id).await.is_err() {logging::warn!("Cannot delete");};
+                    set_done.update(|x| *x += 1);
+                });
+            }>
+                Delete
+            </button>
+        </div>
+    }
+}
+
+#[component]
+pub fn NewEntry(set_done: WriteSignal<usize>) -> impl IntoView {
+    use leptos::html::Input;
+    let now = Local::now().date_naive().to_string();
+
+    let start_date_ref = create_node_ref::<Input>();
+    let start_time_ref = create_node_ref::<Input>();
+    let title_ref = create_node_ref::<Input>();
+
+    view! {
+        <div class="p-6 mx-auto px-8 w-80 space-x-4 rounded-xl shadow-lg flex bg-slate-100">
+            <form class="space-y-4"
+                on:submit=move |ev| {
+                    ev.prevent_default(); // don't reload the page...
+                    let title = title_ref.get().expect("title to exist");
+                    let start_date = start_date_ref.get().expect("start_date to exist");
+                    let start_date =  start_date.value().parse::<NaiveDate>().expect("this to be correct");
+
+                    let start_time = start_time_ref.get().expect("there to be a start time");
+                    let start_time = if let Ok(time) = start_time.value().parse::<NaiveTime>() {Some(time)} else {None};
+
+                    spawn_local(async move {
+                        add_calendar_entry(title.value(), start_date, start_time, None, None).await.expect("this to work");
+                        title.set_value("");
+                        set_done.update(|x| *x += 1);
+                    });
+                }
+            >
+                <label class="text-xl font-medium mx-auto">
+                "Neuer Termin"
+                </label>
+                    <div>
+                        <input class="" type="text" name="title" node_ref=title_ref/>
+                    </div>
+                    <div class="">
+                        <input type="date" name="start_date" value=&now min=&now node_ref=start_date_ref/>
+                        <input type="time" name="start_time" node_ref=start_time_ref/>
+                    </div>
+                    <br/>
+                    <div class="mx-auto flex items-end break-before-all">
+                        <button class="mx-auto ring-2 w-12 rounded-xl" type="submit">"Add"</button>
+                    </div>
+            </form>
         </div>
     }
 }
@@ -121,7 +164,8 @@ async fn add_calendar_entry(
         .execute(&mut conn)
         .await
     {
-        Ok(_row) => Ok(()),
+        Ok(_row) => {
+            Ok(())},
         Err(e) => Err(ServerFnError::ServerError(e.to_string())),
     }
 }
